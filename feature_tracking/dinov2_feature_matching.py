ce4d8a11593b16f33639f7221523743da1fbbb14
Jiacheng <yuanx320@umn.edu>
2023-08-05 18:10:29 -0400

    WIP dinov2 feature matching

--- feature_tracking/dinov2_feature_matching.py
+++ feature_tracking/dinov2_feature_matching.py
@@ -6,54 +6,70 @@ import argparse
 import time
 import torch
 
-def augment_img(img):
-    '''
-    Crop and shift image every 3 pixels
-    '''
-    h, w = img.shape[0], img.shape[1]
-    h = (h - h%14) - 14
-    w = (w - w%14) - 14
+class FeatureExtractor:
+    def __init__(self, dino_model, img) -> None:
+        '''
+        Args:
+            dino_model -- DINO v2 model
+            img -- (numpy.ndarray) shape (w, h, 3)
+        '''
+        self.dino_model = dino_model
+        self.dino_transform = T.Compose([
+                T.Normalize(mean=[0.5], std=[0.5]),
+                ])
+        h, w = img.shape[0], img.shape[1]
+        self.crop_h = (h - h%14) - 14
+        self.crop_w = (w - w%14) - 14
+        self.patch_h, self.patch_w = self.crop_h // 14, self.crop_w // 14
 
-    offset = np.stack(np.meshgrid(np.linspace(0, 15, 6), 
-                                  np.linspace(0, 15, 6),
-                                  indexing='ij'),
-                     axis=-1).reshape(-1, 2).astype('uint8')
+        self.offset = np.stack(np.meshgrid(np.linspace(0, 15, 6), 
+                                    np.linspace(0, 15, 6),
+                                    indexing='ij'),
+                        axis=-1).reshape(-1, 2).astype('uint8')
+        self.batch_size = self.offset.shape[0]
+        self.grid_size = int(np.sqrt(self.batch_size))
+        
 
-    aug_img = np.zeros((36, h, w, 3))
-    for i, (dh, dw) in enumerate(offset):
-        aug_img[i] = img[dh:h+dh, dw:w+dw]
+    def augment_img(self, img):
+        '''
+        Crop and shift image every 3 pixels
+        '''
+        aug_img = np.zeros((36, self.crop_h, self.crop_w, 3))
+        for i, (dh, dw) in enumerate(self.offset):
+            aug_img[i] = img[dh:self.crop_h+dh, dw:self.crop_w+dw]
 
-    aug_img = torch.tensor(aug_img.transpose(0, 3, 1, 2)).cuda()
-    return aug_img
+        aug_img = torch.tensor(aug_img.transpose(0, 3, 1, 2)).cuda()
+        return aug_img
+
+    def extract_feature(self, img):
+        img_tensor = self.augment_img(img)
+        dino_im = self.dino_transform(img_tensor).float().cuda()
+        with torch.no_grad():
+            ret = self.dino_model.forward_features(dino_im)
+            patch_tok = ret["x_norm_patchtokens"].view(self.batch_size, 
+                                                       self.patch_h, 
+                                                       self.patch_w, 
+                                                       -1)
+        return patch_tok
 
 
 def main(args):
     cap = cv2.VideoCapture(args.video)
-    dinov2_vitb14 = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14').cuda().eval()
+    dinov2_model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14').cuda().eval()
 
     ret, first_frame = cap.read()
+    feature_extractor = FeatureExtractor(dinov2_model, first_frame)
 
-    img_tensor = augment_img(first_frame)
-
-    dino_transform = T.Compose([
-            T.Normalize(mean=[0.5], std=[0.5]),
-            ])
+    # Extract features in the first frame
+    initial_feature = feature_extractor.extract_feature(first_frame)
 
     for i in range(20):
         now = time.time()
-        dino_im = dino_transform(img_tensor).float().cuda()
-        ret = dinov2_vitb14.forward_features(dino_im)
+        _, frame = cap.read()
+        feature = feature_extractor.extract_feature(frame)
+
         print("Feature extraction takes: %.2fms"%(1e3 * (time.time() - now)))
         
-    # patch_tok = ret["x_norm_patchtokens"]
-    # cls_tok = ret["x_norm_clstoken"]
-
-    # _, _, img_h, img_w = dino_im.shape
-    # patch_h, patch_w = img_h // 14, img_w // 14
-
-    # patch_tok = einops.rearrange(patch_tok, "b (h w) c -> b c h w", h=patch_h)
-    # cls_tok = einops.repeat(cls_tok, "b c -> b c h w", h=patch_h, w=patch_w)
-    # dino_features = torch.cat((patch_tok, cls_tok), dim=1)[0].permute(1,2,0).detach().numpy()
 
 
 if __name__ == "__main__":

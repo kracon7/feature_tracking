0fdb2b828feadf7dc2ad867c72c4ebd340aa56b0
Jiacheng <yuanx320@umn.edu>
2023-08-07 12:56:04 -0400

    Dense matching is very costly

--- feature_tracking/dinov2_feature_matching.py
+++ feature_tracking/dinov2_feature_matching.py
@@ -14,6 +14,10 @@ class FeatureExtractor:
             img -- (numpy.ndarray) shape (w, h, 3)
         '''
         self.dino_model = dino_model
+        if next(dino_model.parameters()).is_cuda:
+            self.device = 'cuda'
+        else:
+            raise Exception("DINO V2 model is not on GPU!!")
         self.embed_dim = dino_model.embed_dim
         self.dino_transform = T.Compose([
                 T.Normalize(mean=[0.5], std=[0.5]),
@@ -21,16 +25,37 @@ class FeatureExtractor:
         h, w = img.shape[0], img.shape[1]
         self.crop_h = (h - h%14) - 14
         self.crop_w = (w - w%14) - 14
-        self.patch_h, self.patch_w = self.crop_h // 14, self.crop_w // 14
-
         self.stride = 2
+        self.patch_h, self.patch_w = self.crop_h // 14, self.crop_w // 14
+        self.dense_patch_h, self.dense_patch_w = self.crop_h // self.stride, self.crop_w // self.stride
+        
         self.offset = np.stack(np.meshgrid(np.arange(0, 14, self.stride), 
                                     np.arange(0, 14, self.stride),
                                     indexing='ij'),
                         axis=-1).reshape(-1, 2).astype('uint8')
         self.batch_size = self.offset.shape[0]
         self.grid_size = int(np.sqrt(self.batch_size))
-        
+
+        # Search window size
+        self.win_size = 10
+        self.roll_mask = torch.zeros((self.dense_patch_h, 
+                                      self.dense_patch_w,
+                                      self.win_size + 1,
+                                      self.win_size + 1,
+                                      self.embed_dim)).bool().to(self.device)
+        self.roll_shifts = []
+        for i in range(-self.win_size, self.win_size+1, 1):
+            for j in range(-self.win_size, self.win_size+1, 1):
+                self.roll_shifts.append((i, j))
+                if i > 0:
+                    self.roll_mask[:i,:,i,j,:] = 1
+                elif i < 0:
+                    self.roll_mask[i:,:,i,j,:] = 1
+
+                if j > 0:
+                    self.roll_mask[:,:j,i,j,:] = 1
+                elif j < 0:
+                    self.roll_mask[:,j:,i,j,:] = 1
 
     def augment_img(self, img):
         '''
@@ -59,6 +84,23 @@ class FeatureExtractor:
                              dim=0)
         return feature
 
+    def roll_feature(self, feature):
+        '''
+        Arg:
+            feature -- (torch.tensor) shape hf x wf x df
+        Return:
+            rolled -- (torch.tensor) shape hf x wf x hr x wr x df
+        '''
+        rolled = torch.zeros(self.roll_mask.shape).float().to(self.device)
+        for i, j in self.roll_shifts:
+            rolled[:,:, i,j] = torch.roll(feature, (i,j), (0,1))
+        
+        rolled[self.roll_mask] = 0
+        return rolled
+
+    def patch_idx_to_image_idx(self, patch_idx):
+        pass
+
 
 def main(args):
     cap = cv2.VideoCapture(args.video)
@@ -76,6 +118,8 @@ def main(args):
         feature = feature_extractor.extract_feature(frame)
 
         print("Feature extraction takes: %.2fms"%(1e3 * (time.time() - now)))
+
+        rolled = feature_extractor.roll_feature(feature)
 
 
 if __name__ == "__main__":

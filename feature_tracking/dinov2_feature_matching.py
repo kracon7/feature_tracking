97f97e20bb52003a66efd3dc9aa04b1009be5369
Jiacheng <yuanx320@umn.edu>
2023-08-07 14:37:31 -0400

    Cross frame dense matching using DINO V2

--- feature_tracking/dinov2_feature_matching.py
+++ feature_tracking/dinov2_feature_matching.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import torchvision.transforms as T
 import cv2
 import numpy as np
@@ -22,42 +24,27 @@ class FeatureExtractor:
         self.dino_transform = T.Compose([
                 T.Normalize(mean=[0.5], std=[0.5]),
                 ])
+        self.cossim = nn.CosineSimilarity(dim=-1, eps=1e-6)
+
         h, w = img.shape[0], img.shape[1]
         self.crop_h = (h - h%14) - 14
         self.crop_w = (w - w%14) - 14
         self.stride = 2
         self.patch_h, self.patch_w = self.crop_h // 14, self.crop_w // 14
-        self.dense_patch_h, self.dense_patch_w = self.crop_h // self.stride, self.crop_w // self.stride
+        # Densified  embeding shape
+        self.dpatch_h, self.dpatch_w = self.crop_h // self.stride, self.crop_w // self.stride
         
         self.offset = np.stack(np.meshgrid(np.arange(0, 14, self.stride), 
                                     np.arange(0, 14, self.stride),
                                     indexing='ij'),
-                        axis=-1).reshape(-1, 2).astype('uint8')
+                        axis=-1).reshape(-1, 2).astype('int')
         self.batch_size = self.offset.shape[0]
         self.grid_size = int(np.sqrt(self.batch_size))
 
         # Search window size
         self.win_size = 10
-        self.roll_mask = torch.zeros((self.dense_patch_h, 
-                                      self.dense_patch_w,
-                                      self.win_size + 1,
-                                      self.win_size + 1,
-                                      self.embed_dim)).bool().to(self.device)
-        self.roll_shifts = []
-        for i in range(-self.win_size, self.win_size+1, 1):
-            for j in range(-self.win_size, self.win_size+1, 1):
-                self.roll_shifts.append((i, j))
-                if i > 0:
-                    self.roll_mask[:i,:,i,j,:] = 1
-                elif i < 0:
-                    self.roll_mask[i:,:,i,j,:] = 1
 
-                if j > 0:
-                    self.roll_mask[:,:j,i,j,:] = 1
-                elif j < 0:
-                    self.roll_mask[:,j:,i,j,:] = 1
-
-    def augment_img(self, img):
+    def densify(self, img):
         '''
         Crop and shift image every 3 pixels
         '''
@@ -69,7 +56,7 @@ class FeatureExtractor:
         return aug_img
 
     def extract_feature(self, img):
-        img_tensor = self.augment_img(img)
+        img_tensor = self.densify(img)
         dino_im = self.dino_transform(img_tensor).float().cuda()
         with torch.no_grad():
             ret = self.dino_model.forward_features(dino_im)
@@ -79,27 +66,38 @@ class FeatureExtractor:
                                                        self.embed_dim)
         patch_tok = patch_tok.permute([1,2,0,3]).reshape(
                 self.patch_h, self.patch_w, self.grid_size, self.grid_size, self.embed_dim)
-        feature = torch.cat([torch.cat(list(patch_tok[i]), dim=1) 
+        embedings = torch.cat([torch.cat(list(patch_tok[i]), dim=1) 
                              for i in range(self.patch_h)], 
                              dim=0)
-        return feature
+        return embedings
 
-    def roll_feature(self, feature):
+    def calculate_flow(self, old_embedings, new_embedings, old_locs, threshold=0.9):
         '''
-        Arg:
-            feature -- (torch.tensor) shape hf x wf x df
-        Return:
-            rolled -- (torch.tensor) shape hf x wf x hr x wr x df
+        Window search between old_embedings and new_embedings at locations
+        defined by locs.
+        Args:
+            old_embedings -- (torch.tensor) embedings from the previous frame
+            new_embedings -- (torch.tensor) embedings from the next frame
+            locs -- (np.ndarray) n x 2 locations
         '''
-        rolled = torch.zeros(self.roll_mask.shape).float().to(self.device)
-        for i, j in self.roll_shifts:
-            rolled[:,:, i,j] = torch.roll(feature, (i,j), (0,1))
-        
-        rolled[self.roll_mask] = 0
-        return rolled
-
-    def patch_idx_to_image_idx(self, patch_idx):
-        pass
+        old_embedings = old_embedings[old_locs[:,0], old_locs[:,1]].unsqueeze(1)
+        pad = [0,0, self.win_size, self.win_size, self.win_size, self.win_size]
+        padded_new_embedings = F.pad(new_embedings, pad, mode='constant', value=0)
+        new_embedings_windows = []
+        for i, j in old_locs:
+            window = padded_new_embedings[i:i+2*self.win_size+1, 
+                                          j:j+2*self.win_size+1]
+            new_embedings_windows.append(window.reshape((-1, self.embed_dim)))
+        new_embedings_windows = torch.stack(new_embedings_windows)
+        # Cos similarity between one feature and window
+        similarity = self.cossim(old_embedings, new_embedings_windows)
+        top_similarity, match_idx = torch.max(similarity, dim=1)
+        match_idx = torch.argmax(similarity, dim=1)
+        shift = torch.stack([match_idx // (2*self.win_size+1), 
+                             match_idx % (2*self.win_size+1)]).T
+        new_locs = old_locs + shift.cpu().numpy()
+        status = top_similarity > threshold
+        return new_locs, status
 
 
 def main(args):
@@ -110,16 +108,27 @@ def main(args):
     feature_extractor = FeatureExtractor(dinov2_model, first_frame)
 
     # Extract features in the first frame
-    initial_feature = feature_extractor.extract_feature(first_frame)
+    old_embedings = feature_extractor.extract_feature(first_frame)
+
+    locs = np.stack(np.meshgrid(np.arange(0, feature_extractor.dpatch_h, 10), 
+                                    np.arange(0, feature_extractor.dpatch_w, 10),
+                                    indexing='ij'),
+                        axis=-1).reshape(-1, 2).astype('int')
 
     for i in range(20):
         now = time.time()
         _, frame = cap.read()
-        feature = feature_extractor.extract_feature(frame)
+        new_embdeings = feature_extractor.extract_feature(frame)
 
         print("Feature extraction takes: %.2fms"%(1e3 * (time.time() - now)))
+        
+        now = time.time()
+        new_locs, status = feature_extractor.calculate_flow(old_embedings, 
+                                                            new_embdeings,
+                                                            locs)
+        print("Feature matching takes: %.2fms"%(1e3 * (time.time() - now)))
+        old_embedings = new_embdeings
 
-        rolled = feature_extractor.roll_feature(feature)
 
 
 if __name__ == "__main__":
